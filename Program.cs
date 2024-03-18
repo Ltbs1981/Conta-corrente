7f6948e406fea5beb006e32597d6105788e14d01
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-18 02:12:08 -0300

    Iniciando o programa com o SWITCH

--- Program.cs
+++ Program.cs
@@ -6,7 +6,29 @@ namespace ContaCorrente
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double saldo = 3500;
+            
+            Console.WriteLine("Boas vindas ao Banco do Futuro");
+            Console.WriteLine("Escolha a sua operação:\n Consultar saldo, digite 1\n Fazer uma transferência, digite 2\n Sair, digite 3");
+            int acao = int.Parse(Console.ReadLine());
+
+            switch (acao)
+            {
+                case 1:
+                    Console.WriteLine(saldo);
+                    break;
+                case 2:
+                    Console.WriteLine("Para onde você quer tranferi?");
+                    break;
+                case 3:
+                    Console.WriteLine("Obrigado..");
+                    break;
+                default:
+                    Console.WriteLine("Digite uma opção válida");
+                    break;
+
+            }
+
         }
     }
 }
