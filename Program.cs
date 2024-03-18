55d8b91acf5d780f09343c3127a9f81eaaea99d7
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-18 10:51:22 -0300

    Inserido o while

--- Program.cs
+++ Program.cs
@@ -6,28 +6,33 @@ namespace ContaCorrente
     {
         static void Main(string[] args)
         {
+            int acao = 0;
             double saldo = 3500;
-            
-            Console.WriteLine("Boas vindas ao Banco do Futuro");
-            Console.WriteLine("Escolha a sua operação:\n Consultar saldo, digite 1\n Fazer uma transferência, digite 2\n Sair, digite 3");
-            int acao = int.Parse(Console.ReadLine());
 
-            switch (acao)
+            do
             {
-                case 1:
-                    Console.WriteLine(saldo);
-                    break;
-                case 2:
-                    Console.WriteLine("Para onde você quer tranferi?");
-                    break;
-                case 3:
-                    Console.WriteLine("Obrigado..");
-                    break;
-                default:
-                    Console.WriteLine("Digite uma opção válida");
-                    break;
 
-            }
+                Console.WriteLine("Boas vindas ao Banco do Futuro");
+                Console.WriteLine("Escolha a sua operação:\n Consultar saldo, digite 1\n Fazer uma transferência, digite 2\n Sair, digite 3");
+                acao = int.Parse(Console.ReadLine());
+
+                switch (acao)
+                {
+                    case 1:
+                        Console.WriteLine(saldo);
+                        break;
+                    case 2:
+                        Console.WriteLine("Para onde você quer transferir ?");
+                        break;
+                    case 3:
+                        Console.WriteLine("Obrigado..");
+                        break;
+                    default:
+                        Console.WriteLine("Digite uma opção válida");
+                        break;
+
+                }
+            } while (acao != 3);
 
         }
     }
