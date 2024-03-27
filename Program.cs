f8c13396a738a875ee66252511b305cfbedb5a94
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-27 01:52:51 -0300

    Criado o cálculo do saque

--- Program.cs
+++ Program.cs
@@ -7,41 +7,61 @@ namespace ContaCorrente
         static void Main(string[] args)
         {
             int acao = 0;
-            double saldo = 3500;
+            double saldo = 0;
+            Console.WriteLine("Boas vindas ao Banco do Futuro");
+            Console.WriteLine("Informe o seu nome para abrir a sua conta.");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Informe o número da sua conta com 5 dígitos");
+            string numeroConta = Console.ReadLine();
+            Console.WriteLine("Informe o valor de depósito inicial");
+            saldo = double.Parse(Console.ReadLine());
+            Console.WriteLine("***Conta aberta com sucesso***");
 
-            do
+
+            while (acao != 4)
             {
+                Console.WriteLine($"Titular da conta: {nome}\tConta: {numeroConta}\tSaldo: {saldo}");
 
-                Console.WriteLine("Boas vindas ao Banco do Futuro");
-                Console.WriteLine("Escolha a sua operação:\n Consultar saldo, digite 1\n Fazer uma transferência, digite 2\n Sair, digite 3");
-                acao = int.Parse(Console.ReadLine());
+            
+                Console.WriteLine("Escolha a sua operação:\n Consultar saldo, digite 1\n Fazer um saque, digite 2\n Fazer um depósito, digite 3 \n Sair, digite 4");
+            acao = int.Parse(Console.ReadLine());
 
-                switch (acao)
-                {
-                    case 1:
-                        Console.WriteLine(saldo);
-                        break;
-                    case 2:
-                        TransferirValor();
-                        break;
-                    case 3:
-                        Console.WriteLine("Obrigado..");
+            switch (acao)
+            {
+                case 1:
+                    Console.WriteLine(saldo);
+                    break;
+                case 2:
+                    FazerSaque();
+                    break;
+                case 3:
+                    Console.WriteLine("Informe o valor do depósito");
+                    break;
+                    case 4:
+                        Console.WriteLine("Obrigado");
                         break;
                     default:
-                        Console.WriteLine("Digite uma opção válida");
-                        break;
+                    Console.WriteLine("Digite uma opção válida");
+                    break;
 
 
-                         }
-                         void TransferirValor() {
-                            Console.WriteLine("Qual o valor da transferência?");
+            }
+        }
+                void FazerSaque() {
+                            Console.WriteLine("Qual o valor do saque?");
                         double valorTrasferencia = double.Parse(Console.ReadLine());
-                        double novoSaldo =  saldo - valorTrasferencia;
-                        Console.WriteLine(novoSaldo);
-                        saldo = novoSaldo;
+                if (valorTrasferencia > saldo)
+                {
+                    Console.WriteLine("Você não tem saldo suficiente para saque");
+                }
+                else
+                {
+                    double taxa = 5;
+                    saldo -= valorTrasferencia +taxa;
+                }
                          }
-            } while (acao != 3);
+            } 
         }
-        }
+        
     }
 
